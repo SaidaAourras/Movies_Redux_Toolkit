f9944c0bcd6cabe6729eddf6b02f5ae17e98fe52
unknown <Sa123aouras@gmail.com>
2024-10-10 16:02:36 +0100

    db.json movieList Navbar has updated

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,60 +1,69 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMovies } from '../redux/MovieReducer';
-import SingleMovie from './SingleMovie';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies } from "../redux/MovieReducer";
+import SingleMovie from "./SingleMovie";
 import { IoIosStarOutline } from "react-icons/io";
-import './MovieList.css'
+import "./MovieList.css";
 
 const MovieList = () => {
-    const dispatch = useDispatch();
-    const { movies, statut, erreur } = useSelector((state) => state.movies);
-    const categories = Array.from(new Set(movies.map(m => m.category)))
- 
+  const dispatch = useDispatch();
+  const { movies, statut, erreur } = useSelector((state) => state.movies);
+  const categories = Array.from(new Set(movies.map((m) => m.category)));
 
-    useEffect(() => {
-        dispatch(fetchMovies());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
 
-    if (statut === 'loading') {
-        return <div>Loading...</div>;
-    }
+  if (statut === "loading") {
+    return <div>Loading...</div>;
+  }
 
-    if (statut === 'failed') {
-        return <div>Error: {erreur}</div>;
-    }
+  if (statut === "failed") {
+    return <div>Error: {erreur}</div>;
+  }
 
-    return (
-        <div className="d-flex justify-content-around">
-            <div>
-                <div className='radios w-25 mx-auto'>
-                    {
-                        categories.map((cat,index) => (
-                            <div key={index} className="form-check">
-                                <input className="form-check-input" type="radio" name="category"  />
-                                <label className="form-check-label" >
-                                    {cat}
-                                </label>
-                            </div>
-                        ))
-                    }
-
-                </div>
-                <div className='rating'>
-                    <button><IoIosStarOutline /></button>
-                    <button><IoIosStarOutline /></button>
-                    <button><IoIosStarOutline /></button>
-                    <button><IoIosStarOutline /></button>
-                    <button><IoIosStarOutline /></button>
-                </div>
+  return (
+    <div className="container d-flex justify-content-around">
+      <div className="mx-4 my-4">
+        <h1>Categories</h1>
+        <div className="radios">
+          {categories.map((cat, index) => (
+            <div key={index} className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="category"
+              />
+              <label className="form-check-label">{cat}</label>
             </div>
-            <div className="d-flex flex-wrap justify-content-around w-75 mx-auto">
-                {movies.map((movie) => (
-                    <SingleMovie key={movie.id} movie={movie} />
-                ))}
-            </div>
+          ))}
         </div>
-
-    );
+        <div className="rating">
+          <h1>Filter By Rating</h1>
+          <button>
+            <IoIosStarOutline />
+          </button>
+          <button>
+            <IoIosStarOutline />
+          </button>
+          <button>
+            <IoIosStarOutline />
+          </button>
+          <button>
+            <IoIosStarOutline />
+          </button>
+          <button>
+            <IoIosStarOutline />
+          </button>
+        </div>
+      </div>
+      <div className="d-flex flex-wrap justify-content-around w-75 mx-auto">
+        {movies.map((movie) => (
+          <SingleMovie key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MovieList;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,33 @@
-
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Mon Site</Link>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/movies">List Movie</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/watchMovies"> watch Movies </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/watchedMovies"> watched Movies </Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+      <div className="d-flex justify-content-between w-100">
+        <Link className="navbar-brand mx-5" to="/">
+          Mon Site
+        </Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/movies">
+                List Movie
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/watchMovies">
+                {" "}
+                watch Movies{" "}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/watchedMovies">
+                {" "}
+                watched Movies{" "}
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
